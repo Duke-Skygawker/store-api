d28dba1aacb12187d28f45142dd8ccf439c09a65
Duke-Skygawker <bart3kk990@gmail.com>
2021-10-12 09:47:52 +0200

    final commit

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,3 +1,4 @@
-const notFound = (req, res) => res.status(404).send('Route does not exist')
+const notFound = (req, res) =>
+  res.status(404).send("<h1>404</h1><h4>Page Not Found</h4>");
 
-module.exports = notFound
+module.exports = notFound;
